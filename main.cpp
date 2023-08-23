91698716ead848a2efdf3aeb6e843721c6e9d0ca
Dalton Overmyer <overmyerms13dalton@gmail.com>
2023-08-22 20:40:53 -0500

    we are renderering the captured texture :)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include "SDL3/SDL.h"
 #include "capture.h"
 
+std::shared_ptr<SDL_Texture> create_capture_texture(std::shared_ptr<SDL_Renderer> renderer, Capture& capture) {
+  std::shared_ptr<SDL_Surface> surface = std::shared_ptr<SDL_Surface>(SDL_CreateSurfaceFrom(capture.pixels, capture.width, capture.height, capture.stride * 3, SDL_PIXELFORMAT_RGB24), SDL_DestroySurface);
+  return std::shared_ptr<SDL_Texture>(SDL_CreateTextureFromSurface(renderer.get(), surface.get()), SDL_DestroyTexture);
+}
+
 int main() {
   Capture capture;
   if (!capture.capture()) {
@@ -16,7 +21,7 @@ int main() {
     return 1;
   }
 
-  std::shared_ptr<SDL_Window> window = std::shared_ptr<SDL_Window>(SDL_CreateWindow("SDL - Triangulation", capture.width, capture.height, SDL_WINDOW_OPENGL), SDL_DestroyWindow);
+  std::shared_ptr<SDL_Window> window = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Cappy", capture.width, capture.height, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL), SDL_DestroyWindow);
   if (!window) {
     std::cerr << "Failed to create window!\n";
     return 1;
@@ -28,26 +33,40 @@ int main() {
     return 1;
   }
 
-  std::shared_ptr<SDL_Texture> texture;
-  {
-    std::shared_ptr<SDL_Surface> surface = std::shared_ptr<SDL_Surface>(SDL_CreateSurfaceFrom(capture.pixels, capture.width, capture.height, capture.stride * 3, SDL_PIXELFORMAT_RGB24), SDL_DestroySurface);
-    if (!surface) {
-      std::cerr << "Failed to create surface!\n";
-      return 1;
-    }
-    texture = std::shared_ptr<SDL_Texture>(SDL_CreateTextureFromSurface(renderer.get(), surface.get()), SDL_DestroyTexture);
-    if (!texture) {
-      std::cerr << "Failed to create texture!\n";
-      return 1;
-    }
+  std::shared_ptr<SDL_Texture> texture = create_capture_texture(renderer, capture);
+
+  if (!texture) {
+    std::cerr << "Failed to create capture texture!\n";
+    return 1;
   }
 
+  SDL_Event event;
+  bool quit = false;
 
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_EVENT_QUIT:{
+          quit = true;
+          break;
+        }
+        case SDL_EVENT_KEY_DOWN:{
+          if(event.key.keysym.sym == SDLK_q){
+            quit = true;
+            break;
+          }
+          break;
+        }
+      }
+    }
 
+    SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+    SDL_RenderClear(renderer.get());
 
+    SDL_RenderTexture(renderer.get(), texture.get(), NULL, NULL);
 
-
-  // capture.write_pnm("output.pnm");
+    SDL_RenderPresent(renderer.get());
+  }
 
   return 0;
 }
