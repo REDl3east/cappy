2bceb226d9501b9a51e74698e81b9a1660d09622
Dalton Overmyer <overmyerms13dalton@gmail.com>
2025-10-20 17:44:15 -0500

    ditto

--- src/main.c
+++ src/main.c
@@ -495,6 +495,7 @@ static void save_capture(const char* file, app_t* app) {
 
 static void save_dialog_callback(void* userdata, const char* const* filelist, int filter) {
   app_t* app = (app_t*)userdata;
+  
   if (filelist != NULL) {
     if (*filelist == NULL) {
       SDL_Log("Save dialog canceled.");
